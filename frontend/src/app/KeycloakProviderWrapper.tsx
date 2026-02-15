854514efc82bed5ceb7e31851d89cb3a8b7a0bfb
marrubio <mrubio.sopra@grupogimeno.onmicrosoft.com>
2026-02-15 21:38:24 +0100

    ci: Added backend route in deploy

--- frontend/src/app/KeycloakProviderWrapper.tsx
+++ frontend/src/app/KeycloakProviderWrapper.tsx
@@ -3,7 +3,7 @@ import { ReactKeycloakProvider } from '@react-keycloak/web';
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://keycloak-mario-rubio-dev.apps.rm1.0a51.p1.openshiftapps.com/',
+  url: 'http://keycloak/',
   realm: 'testapp',
   clientId: 'testappclient',
 });
